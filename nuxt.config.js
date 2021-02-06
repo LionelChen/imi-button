2d6179d5a6f9f679be9c7c8c65dbbbb57a32efd9
LionelChen <LionelChen>
2021-02-06 03:26:49 +0000

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,150 @@
+import colors from 'vuetify/es5/util/colors';
+
+const is_production = process.env.NODE_ENV === 'production';
+
+export default {
+  mode: 'universal',
+  target: 'static',
+  server: {
+    port: 3000, // default: 3000
+    host: 'localhost' // default: localhost
+  },
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'ころねボタン',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '戌神ころねの音声ボタン 戌神沁音语音按钮 Voice button for Inugami Korone'
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content:
+          'ころねボタン,KORONE BUTTON,沁音按钮,狗按钮,hololive,Inugami Korone,戌神ころね,戌神沁音,沁音,面包狗,饭团猫,狗子,语音按钮,ボイスボタン'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    { src: '@plugins/i18n' },
+    { src: '@plugins/eventBus.js', mode: 'client' },
+    { src: '@plugins/analytics.js', mode: 'client' }
+  ],
+  router: {
+    middleware: 'ui_config'
+  },
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [['@nuxtjs/vuetify', { treeShake: is_production }]],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa', '@nuxtjs/axios', '@nuxtjs/markdownit', '@nuxtjs/sitemap'],
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        light: {
+          primary: '#F2C48D', //orange lighten-3
+          secondary: '#BF8C88',
+          accent: '#BF8C88',
+          error: colors.deepOrange.base,
+          warning: colors.orange.base,
+          info: colors.blueGrey.base,
+          success: colors.teal.base
+        },
+        dark: {
+          primary: '#F2DC99',
+          secondary: '#F26389',
+          accent: '#F26389',
+          error: colors.deepOrange.accent4,
+          warning: colors.amber.base,
+          info: colors.teal.lighten1,
+          success: colors.green.accent3
+        }
+      }
+    },
+    defaultAssets: {
+      font: null,
+      icons: 'mdiSvg'
+    }
+  },
+  pwa: {
+    manifest: {
+      start_url: '/?standalone=true'
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: 'https://cdn.jsdelivr.net/gh/voosc/korone-button@master/static/voices/*'
+        }
+      ]
+    }
+  },
+  markdownit: {
+    preset: 'default',
+    linkify: true,
+    breaks: false,
+    xhtmlOut: true,
+    use: ['markdown-it-div', 'markdown-it-attrs']
+  },
+  sitemap: {
+    path: '/sitemap.xml',
+    hostname: 'https://korone.icu',
+    routes: [
+      {
+        url: '/',
+        changefreq: 'daily',
+        priority: 1,
+        lastmod: new Date()
+      },
+      {
+        url: '/about',
+        changefreq: 'weekly',
+        priority: 0.5,
+        lastmod: new Date()
+      }
+    ]
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    optimizeCSS: is_production,
+    extractCSS: is_production,
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        });
+      }
+    }
+  }
+};
