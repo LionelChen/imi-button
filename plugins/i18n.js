ceb248a35aaa432c8a4b242b30ef039381dbf79e
Lionelchen <chenlinchuan@me.com>
2021-05-15 21:32:35 +0800

    revert changes to i18n

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -3,33 +3,12 @@ import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
-export default {
-  name: 'tour',
-  data () {
-    return {
-      steps: [
-        {
-          target: '#v-step-0',  // We're using document.querySelector() under the hood
-          header: {
-            title: 'Get Started',
-          },
-          content: `Discover <strong>Vue Tour</strong>!`
-        },
-        {
-          target: '.v-step-1',
-          content: 'An awesome plugin made with Vue.js!'
-        },
-        {
-          target: '[data-v-step="2"]',
-          content: 'Try it, you\'ll love it!<br>You can put HTML in the steps and completely customize the DOM to suit your needs.',
-          params: {
-            placement: 'top' // Any valid Popper.js placement. See https://popper.js.org/popper-documentation.html#Popper.placements
-          }
-        }
-      ]
+export default ({ app, store }) => {
+  app.i18n = new VueI18n({
+    locale: store.state.locale,
+    fallbackLocale: 'zh',
+    messages: {
+      zh: require('~/assets/locales/zh.json')
     }
-  },
-  mounted: function () {
-    this.$tours['tour'].start()
-  }
-}
+  });
+};
