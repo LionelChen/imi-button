da8d45da7ac872cdb6ff8bbc2d0dd5743f6ba1d3
Lionelchen <chenlinchuan@me.com>
2021-07-30 15:08:50 +0800

    hover copy keyword

--- pages/emotes.vue
+++ pages/emotes.vue
@@ -1,17 +1,25 @@
 <template>
   <v-row>
     <v-col v-for="emote in emotes" :key="emote.keyword" class="d-flex child-flex">
-      <v-card min-width="125">
-        <v-img
-          :src="`/emotes/${emote.path}`"
-          :lazy-src="`/emotes/${emote.path}`"
-          max-height="125"
-          contain
-          aspect-ratio="1"
-          class="grey lighten-2"
-        ></v-img>
-        <v-card-title class="text-h6"> 关键词:{{ emote.keyword }} </v-card-title>
-      </v-card>
+      <v-hover v-slot="{ hover }">
+        <v-card min-width="125" max-width="200">
+          <v-img
+            :src="`/emotes/${emote.path}`"
+            :lazy-src="`/emotes/${emote.path}`"
+            max-height="125"
+            contain
+            aspect-ratio="1"
+            class="grey lighten-2"
+          >
+            <v-fade-transition>
+              <v-overlay v-if="hover" absolute color="#036358">
+                <v-btn @click.stop="copy_to_clipboard(emote.keyword)">复制关键词</v-btn>
+              </v-overlay>
+            </v-fade-transition>
+          </v-img>
+          <v-card-title class="text-h6 justify-center"> {{ emote.keyword }} </v-card-title>
+        </v-card>
+      </v-hover>
     </v-col>
   </v-row>
 </template>
@@ -24,6 +32,11 @@ export default {
     return {
       emotes: emotes_json.emotes_list
     };
+  },
+  methods: {
+    copy_to_clipboard(keyword) {
+      navigator.clipboard.writeText(keyword);
+    }
   }
 };
 </script>
